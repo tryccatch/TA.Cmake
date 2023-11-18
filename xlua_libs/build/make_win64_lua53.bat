0736a0fa6c6d5756625351bc31d8b1239c283ca0
tryccatch <tryccatch@gmail.com>
2023-11-18 23:17:01 +0800

    Update make_win64_lua53.bat

--- xlua_libs/build/make_win64_lua53.bat
+++ xlua_libs/build/make_win64_lua53.bat
@@ -1,7 +1,9 @@
+set cmake_bin="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
+
 mkdir build64 & pushd build64
-cmake -G "Visual Studio 16 2019" -A x64 ..
+%cmake_bin% -G "Visual Studio 17 2022" -A x64 ..
 popd
-cmake --build build64 --config Release
+%cmake_bin% --build build64 --config Release
 md plugin_lua53\Plugins\x86_64
 copy /Y build64\Release\xlua.dll plugin_lua53\Plugins\x86_64\xlua.dll
 pause
